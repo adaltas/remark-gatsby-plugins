db8473b75b4368d8805211af59eb11664de63649
David Worms <david@adaltas.com>
2020-12-04 11:40:52 +0100

    test(gatsby-remark-title-to-frontmatter): move the title to frontmatter

--- gatsby-remark/title-to-frontmatter/test/index.js
+++ gatsby-remark/title-to-frontmatter/test/index.js
@@ -1,1 +1,34 @@
-console.warn("TODO")
+const Remark = require('remark')
+const toHtml = require('hast-util-to-html')
+const toHast = require('mdast-util-to-hast')
+
+const extractTitle = require('..')
+
+describe( 'Extract title', () => {
+  
+  it( 'Move the title to frontmatter', () => {
+    // Initialize
+    const mast = (new Remark()).parse(
+      [
+        '# this is the title',
+        'and some text'
+      ].join('\n')
+    )
+    const frontmatter = {}
+    // Run
+    extractTitle({
+      markdownNode: {
+        frontmatter: frontmatter
+      },
+      markdownAST: mast
+    }, {})
+    // Convert
+    const hast = toHast(mast)
+    const html = toHtml(hast)
+    // Assert
+    html.should.eql('<p>and some text</p>')
+    frontmatter.should.eql({
+      title: 'this is the title'
+    })
+  })
+})
